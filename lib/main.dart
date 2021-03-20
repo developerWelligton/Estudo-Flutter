b2834c28774d73e1e5f7524a9ffd6f4b53b3b75b
Welligton <developerwelligton@gmail.com>
2021-03-20 11:57:37 -0300

    Estado global(StatefulWidget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,45 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'app_widget.dart';
 
 main() {
-  runApp(appWidget());
+  runApp(estadoGlobalLess()); // runApp
 }
-//teste123
+
+class estadoGlobalLess extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: estadoGlobalful(),
+    );
+  }
+}
+
+/*
+        MaterialApp
+
+        a.	Theme: Esta propriedade considera a classe 
+        ThemeData como o objeto para descrever o tema do MaterialApp. 
+
+         
+        b.	Home: Esta propriedade considera o widget como o objeto a 
+        ser mostrado na rota padrão do aplicativo
+        
+        */
+class estadoGlobalful extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    //criar outra classe
+    return SuporteState();
+  }
+}
+
+//<estadoGlobalful> É REFERENCIA
+class SuporteState extends State<estadoGlobalful> {
+  @override
+//INCRIMENTAR BUILD
+  Widget build(BuildContext context) {
+    return
+        //CONTAINER/TELA
+        Container(child: Text("TESTEFUL"));
+  }
+}
